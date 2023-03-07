aa4d858e78c5ac24f9968a4b9bf95b606016f066
Fredrik Welander <fredrik.welander@subsite.fi>
2023-03-07 23:41:19 +0200

    create_users working

--- cPouta/teacher/create_users.bash
+++ cPouta/teacher/create_users.bash
@@ -1,13 +1,30 @@
 #!/bin/bash
+
+## 
+# A silly script to create multiple users on a server
+#
+# - add usernames to .env
+# - while testing, to completely rempove a user:
+#     sudo deluser --remove-home username
+##
+
+if [ "$USER" != "root" ]
+then
+    echo "Run this as root or with sudo"
+    exit 1
+fi
+
 . ./.env
+
 for user in ${USERLIST//,/ }
 do
-    echo "---- Creating user: $user"
-    PASS=$(openssl rand -hex 6)
-    useradd -m -s /bin/bash $user
-    echo "$user:$PASS" | chpasswd
-    
+        echo "---- Creating user: $user"
+        PASS=$(openssl rand -hex 6)
+        useradd -m -s /bin/bash $user
+        echo "$user:$PASS" | chpasswd
+        curl -X POST -H "Content-Type: application/json" -d \
+                '{ "to": "'$user'@arcada.fi", "subject": "Your credentials", "body": "'$user' / '$PASS'" }' \
+                $MAILER_URL
+                
 done
-
-# sudo deluser  --remove-home welandfr && sudo deluser --remove-home fredrik.welander
 
