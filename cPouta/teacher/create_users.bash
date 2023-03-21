c97cb4bf7eebe4e89b658841763412c832829857
Fredrik Welander <welandfr@arcada.fi>
2023-03-21 15:48:45 +0200

    add check for existing user

--- cPouta/teacher/create_users.bash
+++ cPouta/teacher/create_users.bash
@@ -18,13 +18,17 @@ fi
 
 for user in ${USERLIST//,/ }
 do
-        echo "---- Creating user: $user"
+    echo "---- Creating user: $user"
+    if id "$user" &>/dev/null; then
+        echo "NOTICE: User $user already exists, skipping..."
+    else
         PASS=$(openssl rand -hex 6)
         useradd -m -s /bin/bash $user
         echo "$user:$PASS" | chpasswd
         curl -X POST -H "Content-Type: application/json" -d \
                 '{ "to": "'$user'@'$MAILDOMAIN'", "subject": "Your credentials", "body": "'$user' / '$PASS'" }' \
                 $MAILER_URL
+    fi
 
 done
 
